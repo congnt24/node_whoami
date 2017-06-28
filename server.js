9f34312c07ed5f8b97c677cb2c14d81f68789937
System <none>
2017-06-28 10:28:25 +0000

    🐵😥 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,49 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/whoami", function (request, response) {
+  //'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+  
+  
+  var output = {"ipaddress":"","language":"","software":""}
+  output['ipaddress'] = request.headers['x-forwarded-for'].split(',')[0]
+  output['language'] = request.headers['accept-language'].split(',')[0]
+  output['software'] = request.headers['user-agent'].split('(')[1].split(')')[0]
+  
+  // console.log(software)
+  response.end(JSON.stringify(output))
+  // response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
